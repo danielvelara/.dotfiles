54708ebd94d99032dfca65d8e499025c1e85617b
Daniel E. Velázquez Lara <13500134+danielvelara@users.noreply.github.com>
2024-01-11 11:06:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ yay -S elixir inotify-tools
 yay -S sqlite litecli sqlitebrowser pgcli dbeaver usql postgresql # Databases
 
 # Cloud
-yay -S docker docker-compose lazydocker-bin dive aws-cli-v2-bin traefik-bin portmaster-stub-bin flyctl-bin # DevOps
+yay -S docker docker-compose lazydocker-bin ctop dive aws-cli-v2-bin traefik-bin portmaster-stub-bin flyctl-bin # DevOps
 sudo usermod -aG docker $USER
 npm i -g serve
 npm i -g serverless
