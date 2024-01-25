886dda31cb02b3066dd20d8421c720cec89945cd
Daniel E. Velázquez Lara <13500134+danielvelara@users.noreply.github.com>
2024-01-25 14:21:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,79 +15,178 @@ fi
 sleep 1s
 clear
 
-# Install Nix
+
+########################
+## Nix
+########################
+
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
-# yay
 pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-# System
-ln -sfn ~/.dotfiles/.xinitrc .xinitrc
-ln -sfn ~/.dotfiles/.Xresources ~/
-xrdb -load ~/.Xresources
-ln -sfn ~/.dotfiles/.xinitrc .xinitrc
-ln -sfn ~/.dotfiles/.config/mimeapps.list ~/.config
-# yay -S  betterlockscren
-# yay -S xautolock
-# ln -sfn ~/.dotfiles/.profile .profile
 
-# Terminal
-yay -S alacritty zsh ttf-meslo-nerd-font-powerlevel10k tmux
-ln -sfn ~/.dotfiles/.config/alacritty ~/.config
+########################
+## System
+########################
+
+yay -S \
+ 	zsh \
+  	tmux \
+  	alacritty \
+  	ttf-meslo-nerd-font-powerlevel10k \
+   	
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-# ln -sfn ~/.dotfiles/.p10k.zsh ~/
-ln -sfn ~/.dotfiles/.tmux.conf ~/
 
-# i3wm
-yay -S polybar dunst rofi rofi-emoji rofi-calc redshift xorg-xrandr arandr autorandr
-ln -sfn ~/.dotfiles/.config/polybar ~/.config
+ln -sfn ~/.dotfiles/.xinitrc ~/
+ln -sfn ~/.dotfiles/.newsboat ~/
+ln -sfn ~/.dotfiles/.gitconfig ~/
+ln -sfn ~/.dotfiles/.tmux.conf ~/
+ln -sfn ~/.dotfiles/.Xresources ~/
+ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/.dotfiles/.profile .profile
+ln -sfn ~/.dotfiles/.config/lf ~/.config
+ln -sfn ~/.dotfiles/.config/nvim ~/.config
+ln -sfn ~/.dotfiles/.config/xplr ~/.config
 ln -sfn ~/.dotfiles/.config/dunst ~/.config
 ln -sfn ~/.dotfiles/.config/rofi/ ~/.config
+ln -sfn ~/.dotfiles/.config/polybar ~/.config
+ln -sfn ~/.dotfiles/.config/alacritty ~/.config
+ln -sfn ~/.dotfiles/.config/ncmpcpp/config ~/.config/ncmpcpp
+ln -sfn ~/.dotfiles/.config/ncmpcpp/bindings ~/.config/ncmpcpp
+ln -sfn ~/.dotfiles/.config/mpd/mpd.conf ~/.config/mpd/mpd.conf
+ln -sfn ~/.dotfiles/.config/mpd/playlist ~/.config/mpd/playlist
+ln -sfn ~/.dotfiles/.config/VSCodium/settings.json ~/.config/VSCodium/User
+
+########################
+## Fonts
+########################
+
+pacman -S \
+ 	ttf-firacode \
+  	ttf-ms-fonts \
+	ttf-liberation \
+   	noto-fonts noto-fonts-emoji
+
+########################
+## Git
+########################
+
+yay -S git lazygit github-cli git-chglog onefetch  git-delta
+echo "function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/\$@ ;}" >>~/.zshrc && source ~/.zshrc
+
+
+########################
+## i3wm
+########################
+
+yay -S \
+ 	dunst \
+  	polybar \
+  	redshift \
+    	lxappearence \
+	rofi rofi-emoji rofi-calc \
+    	xorg-xrandr arandr autorandr
 
 yay -S bluez bluez-utils cups
 systemctl enable bluetooth.service --now
 systemctl enable cups.service --now
 
-# CLI
-yay -S pass pass-otp exa fd bat tldr navi ripgrep fzf jump dust csview czkawka-cli git-delta hexyl tokei
-yay -S neofetch visidata ncdu bottom duf gdu ncdu gparted testdisk nvtop hyperfine stress-ng
-yay -S 7zip cfdisk qrcp libqalculate
-yay -S man-db perl-image-exiftool mediainfo
-yay -S xclip cronie screenkey
-yay -S power-profiles-daemon brightnessctl feh
-yay -S gpg-tui age
+########################
+## Applications
+########################
 
-# Fonts
-pacman -S ttf-liberation ttf-firacode ttf-ms-fonts noto-fonts noto-fonts-emoji
+yay -S \
+	thunar thunar-archive-plugin xarchiver unzip gvfs gvfs-mtp \
+	brave-bin \
+ 	librewolf-bin \
+  	anki-bin \
+   	vscodium-bin \
+   	calibre \
+    	imv \
+     	mpv \
+	zathura zathura-pdf-mupdf \
+	obs-studio \
+ 	gimp \
+  	handbrake \
+   	imagemagick \
+    	flameshot
 
-yay -S vscodium-bin
-ln -sfn ~/.dotfiles/.config/VSCodium/product.json ~/.config/VSCodium # Enable Marketplace
-ln -sfn ~/.dotfiles/.config/VSCodium/keybindings.json ~/.config/VSCodium/User
-ln -sfn ~/.dotfiles/.config/VSCodium/settings.json ~/.config/VSCodium/User
-ln -sfn ~/.dotfiles/.config/VSCodium/snippets ~/.config/VSCodium/User
+########################
+## CLI
+########################
 
-# Git
-yay -S git lazygit github-cli git-chglog onefetch
-ln -sfn ~/.dotfiles/.gitconfig ~/
-echo "function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/\$@ ;}" >>~/.zshrc && source ~/.zshrc
+yay -S \
+	pass pass-otp \
+   	fzf \
+   	bat \
+   	duf \
+    	gdu \
+     	age \
+    	tldr \
+     	navi \	
+ 	jump \
+  	dust \
+        cfdisk \
+   	ncdu \
+    	tokei \
+   	csview \
+  	bottom \
+     	gparted \
+    	ripgrep \
+	testdisk \
+	neofetch \
+ 	visidata \
+    	czkawka-cli \
+ 	nvtop \
+  	hyperfine \
+   	stress-ng \
+    	7zip \
+      	qrcp \
+     	feh \
+    	xclip \
+     	cronie \
+       	man-db \
+      	gpg-tui \
+      	screenkey \
+	mediainfo \
+ 	libqalculate \
+ 	brightnessctl \
+  	perl-image-exiftool \
+	power-profiles-daemon \
+    	
+	scrcpy \
+ 	hledger-bin \
+  	newsboat \
+      	slides-bin \
+	lf-bin \
+ 	xplr \
+  	ncmpcpp mpc mpd pulsemixer \
+  	# calcurse \
+	# pomo \
+ 	# slidev-bin \
+  	# khal \
+  	pandoc-bin beamer-bin texlive-core
 
-# Neovim
-yay -S neovim
-ln -sfn ~/.dotfiles/.config/nvim ~/.config
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
-# Developer
-yay -S python python-pip asdf goreleaser-bin tinygo-bin
+
+########################
+## Developer
+########################
+
+yay -S \
+	base-devel \
+ 	hugo \
+  	asdf \
+   	goreleaser-bin
+# npm inotify-tools python python-pip   
 pip install --user pipx
-pipx ensurepath
-pipx install poetry
+pipx install poetry ensurepath yt-dlp python-spotdl
 
 asdf plugin add erlang && asdf install erlang latest && asdf global erlang latest
 asdf plugin add elixir && asdf install elixir latest && asdf global elixir latest
@@ -96,57 +195,66 @@ asdf plugin add nodejs && asdf install nodejs latest && asdf global nodejs lates
 asdf plugin add python && asdf install python latest && asdf global python latest
 # asdf plugin add java
 
-yay -S base-devel tcc hugo
-yay -S npm inotify-tools
+yay -S neovim
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
-# Databases
-yay -S sqlite litecli sqlitebrowser dbeaver usql postgresql pgcli # Databases
+########################
+## Databases
+########################
 
-# Cloud
-yay -S docker docker-compose lazydocker-bin ctop dive aws-cli-v2-bin traefik-bin portmaster-stub-bin flyctl-bin # DevOps
+yay -S \
+	sqlite litecli sqlitebrowser dbeaver usql \
+ 	postgresql pgcli
+
+########################
+## DevOps
+########################
+
+yay -S \
+	docker docker-compose lazydocker-bin ctop dive \
+    	flyctl-bin
+     
 sudo usermod -aG docker $USER
-npm i -g serve
-npm i -g serverless
 
-# AI
-# yay -S ollama whisper.cpp
+########################
+## Networking
+########################
 
-# Networking
-
-yay -S net-tools bind ngrok-bin inetutils aria2 nload ipcalc whois ntop protonvpn ufw gufw httpie insomnia-bin jq websocat static-web-server-bin gping oha tailscale nmap rustscan ipfs intermodal-bin termshark trippy goaccess
-
-# Cybersecurity
-yay -S aircrack-ng sublist3r-git gnu-netcat burpsuite nikto wifite2 nmap
-
-# Applications
-yay -S brave-bin librewolf-bin anki-bin xournalapp calibre
-yay -S imv mpv zathura zathura-pdf-mupdf
-ln -sfn ~/.dotfiles/.config/zathura ~/.config
-yay -S obs-studio gimp handbrake imagemagick flameshot # Media
-
-# GUI
-yay -S thunar thunar-archive-plugin xarchiver unzip gvfs gvfs-mtp scrcpy lxappearence
-ln -sfn ~/.dotfiles/.config/lf ~/.config
-ln -sfn ~/.dotfiles/.config/xplr ~/.config
-
-# Utilities
-yay -S hledger-bin cointop-bin lf-bin xplr
-yay -S pandoc-bin beamer-bin texlive-core slides-bin rustdesk-bin
-# calcurse \
-# pomo # slidev-bin
-# khal \
-
-yay -S newsboat
-ln -sfn ~/.dotfiles/.newsboat ~/
-
-# yay -S python-spotdl yt-dlp \
-# Music
-yay -S ncmpcpp mpc mpd pulsemixer
-ln -sfn ~/.dotfiles/.config/mpd/mpd.conf ~/.config/mpd/mpd.conf
-ln -sfn ~/.dotfiles/.config/mpd/playlist ~/.config/mpd/playlist
-ln -sfn ~/.dotfiles/.config/ncmpcpp/config ~/.config/ncmpcpp
-ln -sfn ~/.dotfiles/.config/ncmpcpp/bindings ~/.config/ncmpcpp
-systemctl enable mpd --now
+yay -S \
+  	jq \
+  	oha \
+  	ufw \
+   	gufw \
+ 	bind \
+	ntop \
+      	ipfs \
+	nmap \
+       	gping \
+	aria2 \
+ 	whois \
+     	nload \
+     	nikto \
+      	ipcalc \
+ 	trippy \
+	httpie \
+       	wifite2 \
+ 	websocat \
+       	rustscan \
+  	goaccess \
+   	net-tools \
+	protonvpn \
+ 	termshark \
+ 	ngrok-bin \
+   	inetutils \
+	tailscale \
+      	burpsuite \
+       	gnu-netcat \
+	aircrack-ng \
+	traefik-bin \
+      	sublist3r-git \
+        intermodal-bin \
+   	portmaster-stub-bin \
+	static-web-server-bin \
 
 echo "Remember to set autorandr config"
 echo "Successfull installation! Reboot? y/n"
